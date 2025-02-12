d48e241022abb49c82b96da3001c8949c651b180
Tuấn Anh <vtabk2@gmail.com>
2025-02-12 21:10:02 +0700

    base activity

--- app/src/main/java/com/gs/test/MainActivity.kt
+++ app/src/main/java/com/gs/test/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.gs.test
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.gs.core.ui.activity.BaseVMActivity
+import com.gs.test.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseVMActivity<ActivityMainBinding>() {
+
+    override fun getViewBinding(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun setupSystemUi(show: Boolean) {
+        super.setupSystemUi(false)
     }
 }
