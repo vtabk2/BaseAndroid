96b12cb216cc5523d34f32998d9b86dedc18d8e4
Tuấn Anh <vtabk2@gmail.com>
2025-02-12 23:02:03 +0700

    main

---
+++ core/src/main/java/com/gs/core/utils/SubActivityLifecycleCallbacks.kt
@@ -0,0 +1,38 @@
+package com.gs.core.utils
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+open class SubActivityLifecycleCallbacks(
+    private val callbackCreated: ((Activity) -> Unit)? = null,
+    private val callbackResumed: ((Activity) -> Unit)? = null
+) : Application.ActivityLifecycleCallbacks {
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        callbackCreated?.invoke(activity)
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        // no-op
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        callbackResumed?.invoke(activity)
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+        // no-op
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        // no-op
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        // no-op
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        // no-op
+    }
+}
