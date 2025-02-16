eecc178efba235478f3a7da0598985425930f9c4
Tuấn Anh <vtabk2@gmail.com>
2025-02-16 23:18:52 +0700

    add decompress file zip

---
+++ core/src/main/java/com/gs/core/utils/CoreConstant.kt
@@ -0,0 +1,10 @@
+package com.gs.core.utils
+
+object CoreConstant {
+    const val ELEVATION_RANK_1 = 10f
+    const val ELEVATION_RANK_2 = 12f
+    const val ELEVATION_RANK_3 = 14f
+    const val ELEVATION_RANK_4 = 16f
+
+    const val LIMIT_LOADED = 100
+}
