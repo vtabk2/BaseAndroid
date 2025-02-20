88e112f592fdab390a2feba5845103c70aa725b3
Tuấn Anh <vtabk2@gmail.com>
2025-02-20 16:56:24 +0700

    extensions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -47,4 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.transitionseverywhere)
 }
