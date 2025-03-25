230553bed1f01e589e3d1ec68d45dd06c3981d79
Tuấn Anh <vtabk2@gmail.com>
2025-03-25 21:33:34 +0700

    update

--- app/src/main/java/com/gs/test/MainActivity.kt
+++ app/src/main/java/com/gs/test/MainActivity.kt
@@ -1,38 +1,8 @@
 package com.gs.test
 
-import android.os.Bundle
-import android.util.Log
-import com.core.gsadmob.natives.AdsMode
-import com.core.gsadmob.natives.NativeUtils
-import com.core.gsadmob.natives.view.BaseNativeAdView
-import com.core.gscore.utils.network.NetworkUtils
 import com.core.gsmvvm.ui.activity.BaseMVVMActivity
 import com.gs.test.databinding.ActivityMainBinding
 
-class MainActivity : BaseMVVMActivity<ActivityMainBinding>() {
+class MainActivity : BaseMVVMActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
-    override fun getViewBinding(): ActivityMainBinding {
-        return ActivityMainBinding.inflate(layoutInflater)
-    }
-
-    override fun initViews(savedInstanceState: Bundle?) {
-        super.initViews(savedInstanceState)
-        TestApplication.applicationContext().initMobileAds()
-
-        bindingView.nativeTest1.applyBuilder(BaseNativeAdView.Builder().apply {
-            adsMode = AdsMode.VIP
-        })
-
-        NativeUtils.loadNativeAds(this, this, isVip = false, callbackStart = {
-            bindingView.nativeTest1.startShimmer()
-        }, callback = { nativeAd ->
-            bindingView.nativeTest1.setNativeAd(nativeAd)
-        })
-
-        NetworkUtils.hasInternetAccessCheck(doTask = {
-            Log.d("TAG5", "initViews: success")
-        }, doException = { networkError ->
-            Log.d("TAG5", "initViews: networkError = " + networkError.name)
-        }, activity = this)
-    }
 }
